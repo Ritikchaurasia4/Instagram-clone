f90f3cc16e6ef872630385c7a6f5cf23b3f09054
ritik chaurasia <ritikchaurasia0707@gmailcom>
2024-10-20 20:31:18 +0530

    Instagram clone

---
+++ app.js
@@ -0,0 +1,56 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var expressSession = require('express-session'); // save the user data on the server 
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const passport = require('passport');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(expressSession({ // With the help of this code the user will be login . means it allows to login , also for data saving
+  resave: false,
+  saveUninitialized: false,
+  secret: "heyheyehhdd"
+}));
+
+app.use(passport.initialize()); // passport initialized , here passport means who will make login , register , protected routes. Here we can say passport are that The person who keeps the passport logged in is the one."
+
+app.use(passport.session()); // Withe the help of this code data are being save.
+
+passport.serializeUser(usersRouter.serializeUser());
+passport.deserializeUser(usersRouter.deserializeUser());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
